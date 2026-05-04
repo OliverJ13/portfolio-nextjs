8776c400037b24516cd49d6acafc9b8fa6f895a8
OliverJ13 <ajelderks07@gmail.com>
2026-05-04 12:00:51 -0700

    Sitemap.js

---
+++ nextjs/my-app/app/sitemap.js
@@ -0,0 +1,20 @@
+export default function sitemap() {
+    return [
+        {
+            url: 'https://portfolio-nextjs-dktm.vercel.app/',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://portfolio-nextjs-dktm.vercel.app/about',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://portfolio-nextjs-dktm.vercel.app/portfolio',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://portfolio-nextjs-dktm.vercel.app/individula-projects',
+            lastModified: new Date(),
+        },
+    ]
+}
