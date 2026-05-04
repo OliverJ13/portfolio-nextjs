0a5e7d86e0149106f0b0d22a56e27a8a61c00ad1
OliverJ13 <ajelderks07@gmail.com>
2026-05-04 12:12:18 -0700

    Google Anylistics

--- nextjs/my-app/app/layout.tsx
+++ nextjs/my-app/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
+
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +30,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <GoogleAnalytics gaId="G-PRLTS5HP9X" />
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
